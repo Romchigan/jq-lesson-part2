9653654716b5a15a29e5275db936c0fc34e7935f
Romchigan <ronaldo.mail97@gmail.com>
2017-01-21 19:17:41 +0200

    add js files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+jQuery(function ($) {
+
+   $('.phone').hover(
+       function () {
+        $(this).css("color", "red")
+       },
+       function () {
+           $(this).css("color", "#92a1a5")
+       });
+
+
+
+});
